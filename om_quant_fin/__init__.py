53e5435fbc3903e66e27b1ba7ce0b1de14be3ca2
Outspoken Market <132607272+outspokenmarket@users.noreply.github.com>
2023-05-04 21:09:20 +0200

    updated

---
+++ om_quant_fin/__init__.py
@@ -0,0 +1,10 @@
+from .om_quant_fin import (
+    download_data,
+    rolling_z_score,
+    rolling_ratio,
+    calculate_returns,
+    split_data,
+    run_logistic_regression,
+    calculate_plot_auc,
+    calculate_gini,
+)
