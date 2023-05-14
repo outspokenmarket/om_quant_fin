22e9fa097de05aec77447c2001efaab77d9d9c1f
Outspoken Market <132607272+outspokenmarket@users.noreply.github.com>
2023-05-14 12:34:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name = "om_quant_fin",
-    version = "1.0.0",
+    version = "1.0.1",
     packages = find_packages(),
     install_requires = [
         "yfinance",
