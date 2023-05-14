605c3fcf7e37bd472f417182cecda24bbec4fb11
Outspoken Market <132607272+outspokenmarket@users.noreply.github.com>
2023-05-14 12:32:43 +0200

    Update 1.0.0

--- om_quant_fin/om_quant_fin.py
+++ om_quant_fin/om_quant_fin.py
@@ -2,8 +2,14 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_curve, roc_auc_score, auc
+from sklearn.metrics import roc_curve, roc_auc_score, auc, accuracy_score
+from sklearn.utils import resample
+from sklearn.base import BaseEstimator, TransformerMixin
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from time import sleep
+import sys
 
 
 def download_data(ticker, start_date, end_date):
@@ -52,52 +58,19 @@ def rolling_ratio(data, window):
     return ratio
 
 
-def calculate_returns(data):
+def calculate_returns(data, period = 1):
     """Calculate returns from price data.
     
     Args:
         data (Series): Input price data.
+        period (Integer): number of periods; 1 as default
         
     Returns:
         Series: Returns.
     """
-    returns = data.pct_change()
+    returns = data.pct_change(period)
     return returns
 
-
-def split_data(data, train_start, train_end, test_start, test_end):
-    """Split data into training and test sets based on date ranges.
-    
-    Args:
-        data (DataFrame): Input data.
-        train_start (str): Start date for training data in the format "YYYY-MM-DD".
-        train_end (str): End date for training data in the format "YYYY-MM-DD".
-        test_start (str): Start date for test data in the format "YYYY-MM-DD".
-        test_end (str): End date for test data in the format "YYYY-MM-DD".
-        
-    Returns:
-        tuple: A tuple containing the training and test data (DataFrames).
-    """
-    train = data[train_start:train_end]
-    test = data[test_start:test_end]
-    return train, test
-
-
-def run_logistic_regression(X_train, y_train, X_test):
-    """Train a logistic regression model and make predictions.
-    
-    Args:
-        X_train (DataFrame): Input features for training data.
-        y_train (Series): Target variable for training data.
-        X_test (DataFrame): Input features for test data.
-        
-    Returns:
-        Series: Predicted values for test data.
-    """
-    model = LogisticRegression()
-    model.fit(X_train, y_train)
-    predictions = model.predict(X_test)
-    return predictions
 
 def calculate_plot_auc(y_true, y_pred, dataset_type = "Unknown"):
     """Calculate and plot the AUC for given true labels and predictions.
@@ -113,18 +86,27 @@ def calculate_plot_auc(y_true, y_pred, dataset_type = "Unknown"):
     fpr, tpr, _ = roc_curve(y_true, y_pred)
     roc_auc = auc(fpr, tpr)
     
-    plt.figure()
-    plt.plot(fpr, tpr, color = "darkorange", lw = 2, label = "ROC curve (area = %0.2f)" % roc_auc)
-    plt.plot([0, 1], [0, 1], color = "navy", lw = 2, linestyle = "--")
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel("False Positive Rate")
-    plt.ylabel("True Positive Rate")
-    plt.title(f"{dataset_type} Set ROC")
-    plt.legend(loc = "lower right")
-    plt.show()
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x = fpr, y = tpr, mode = "lines", name = f"ROC curve (area = {roc_auc:.2f})"))
+    fig.add_trace(go.Scatter(x = [0, 1], y = [0, 1], mode = "lines", name = "Random Classifier"))
+    
+    fig.update_layout(
+        title = f"{dataset_type} Set ROC"
+        , xaxis_title = "False Positive Rate"
+        , yaxis_title = "True Positive Rate"
+        , autosize = False
+        , width = 800
+        , height = 600
+        , legend = dict( x = 0.8, y = 0.0
+                        , bgcolor = "rgba(255, 255, 255, 0)"
+                        bordercolor = "rgba(255, 255, 255, 0)"
+                        )
+        , template = "plotly_white")
+    
+    fig.show()
     
     return roc_auc
+
 
 def calculate_gini(auc_score):
     """Calculate the Gini coefficient based on the AUC.
@@ -137,3 +119,232 @@ def calculate_gini(auc_score):
     """
     gini = 2 * auc_score - 1
     return gini
+
+
+def pain_index(prices_series, freq = 252):
+    """Calculate the Pain Index for a given price series
+    
+    Args:
+        prices_series (float): Price series
+        freq (integer): frequency for the pain index estimation
+        
+    Returns:
+        float: Pain index
+    """
+
+    # Calculate drawdowns
+    peak_value = pd.Series(prices_series).expanding(min_periods = 1).max()
+    drawdowns = (prices_series / peak_value) - 1
+    
+    # Calculate drawdown durations
+    drawdown_durations = (drawdowns < 0).cumsum()
+    drawdown_durations[drawdowns >= 0] = 0
+    
+    # Calculate the product of drawdowns and their durations
+    pain_products = drawdowns * drawdown_durations
+
+    # Calculate the average pain per period
+    average_pain = pain_products.sum() / freq
+
+    # Calculate the Pain Index
+    pain_index = -average_pain
+
+    return pain_index
+
+
+def plot_pain_index(ticker, index, close, pain_index):
+    """Plot the Pain Index and the price series for the given tickers
+    
+    Args:
+        ticker (string): Ticker symbol from Yahoo finance
+        index (datetime): index from the main data frame in a date time format
+        close (data frame column): price series
+        pain_index (data frame column): the pain index
+        
+    Returns:
+        float: Pain index
+    """
+
+    fig = make_subplots(rows = 2, cols = 1
+                        , shared_xaxes = True
+                        , vertical_spacing = 0.05)
+
+    fig.add_trace(go.Scatter(x = index, y = close
+                                    , name = ticker
+                                    , line = dict(color = "blue"))
+                  , row = 1, col = 1)
+
+    fig.add_trace(go.Scatter(x = index, y = pain_index
+                                    , name = "Pain index"
+                                    , line = dict(color = "red"))
+                  , row = 2, col = 1)
+
+    fig.update_layout(height = 800, width = 800
+                      , title = "Pain Index: " + ticker
+                      , font_color = "blue"
+                      , title_font_color = "black"
+                      , xaxis_title = "Time"
+                      , yaxis_title = ticker
+                      , yaxis2_title = "Pain Index"
+                      , font = dict(size = 15, color = "Black")
+                  )
+
+    fig.update_layout(hovermode = "x")
+
+    # Code to exclude empty dates from the chart
+    dt_all = pd.date_range(start = index[0]
+                           , end = index[-1]
+                           , freq = "D")
+    dt_all_py = [d.to_pydatetime() for d in dt_all]
+    dt_obs_py = [d.to_pydatetime() for d in index]
+
+    dt_breaks = [d for d in dt_all_py if d not in dt_obs_py]
+
+    fig.update_xaxes(
+        rangebreaks = [dict(values = dt_breaks)]
+    )
+    return fig.show()
+
+
+def model_bs(model, X_train, y_train, X_test, y_test, n_iterations = 1000, range_bs = 0.1):
+    """Calculates a bootstrapping simulation for model accuracy
+    
+    Args:
+        model (model object): configured classification model
+        X_train (DataFrame): training features
+        y_train (DataFrame): training target
+        X_test (DataFrame): testing features
+        y_test (DataFrame): testing target
+        n_iterations (integer): number of interations for the BS calculation; 1000 is default
+        range (float): percentage of the mean for robustness validation
+        
+    Returns:
+        DataFrame: with accuracies for y_train and y_test
+    """
+    n_iterations = n_iterations
+    accuracy_df = pd.DataFrame()
+    model_bs = model
+
+    for i in range(n_iterations):
+        sys.stdout.write("\r")
+        sys.stdout.write(str(np.round(i/n_iterations*100, 2)) + " %")
+        sys.stdout.flush()
+        sleep(0.005)
+        
+        X_train_resample, y_train_resample = resample(X_train, y_train)
+    
+        model.fit(X_train_resample, y_train_resample)
+        predictions_train = model_bs.predict(X_train)
+        predictions_test = model_bs.predict(X_test)
+        accuracy_train = accuracy_score(y_train, predictions_train)
+        accuracy_test = accuracy_score(y_test, predictions_test)
+
+    # Armazena os resultados no data frame
+        accuracy_df.loc[i, "accuracy_train"] = accuracy_train
+        accuracy_df.loc[i, "accuracy_test"] = accuracy_test
+    
+    print("")
+    print(70*"-")
+    print("Average - training set: " + str(np.round(accuracy_df["accuracy_train"].mean(), 3)))
+    print("Standard Deviation - training set: " + str(np.round(accuracy_df["accuracy_train"].std(), 3)))
+    print("Average - testing set: " + str(np.round(accuracy_df["accuracy_test"].mean(), 3)))
+    print("Standard Deviation - testing set: " + str(np.round(accuracy_df["accuracy_test"].std(), 3)))
+    print(70*"-")
+
+    bs_trigger = range_bs
+    p2_train = accuracy_df["accuracy_train"].quantile(0.02)  # 2nd percentile
+    p98_train = accuracy_df["accuracy_train"].quantile(0.98)  # 98th percentile
+    bs_range_train = p98_train-p2_train
+    bs_mean_train = accuracy_df["accuracy_train"].mean()
+
+    p2_test = accuracy_df["accuracy_test"].quantile(0.02)  # 2nd percentile
+    p98_test = accuracy_df["accuracy_test"].quantile(0.98)  # 98th percentile
+    bs_range_test = p98_test-p2_test
+    bs_mean_test = accuracy_df["accuracy_test"].mean()
+
+    if (bs_range_train < bs_mean_train*bs_trigger):
+        print("Training: Model is robust")
+    else:
+        print("Training: Model is not robust")
+    print(70*"-")
+    print(70*"-")
+
+    if (bs_range_test < bs_mean_test*bs_trigger):
+        print("Test: Model is robust")
+    else:
+        print("Test: Model is not robust")
+    print(70*"-")
+    
+    return accuracy_df
+
+
+def plot_bs(metric_bs):
+    """Plot the distribution of accuracies from the bootstrapping simulation
+    
+    Args:
+        metric_bs (DataFrame): data frame from the model_bs function
+        
+    Returns:
+        histogram with the distribution of the metric of choice
+    """
+    fig = go.Figure(data = [go.Histogram(x = metric_bs)])
+    fig.update_layout(
+        title_text = "Bootstrapping Results - Histogram"
+        , width = 600
+        , height = 600 
+        )
+    fig.show()
+
+
+class QCutTransformer(BaseEstimator, TransformerMixin):
+    """QCutTransformer is a custom transformer class that extends the BaseEstimator and TransformerMixin classes from sklearn.
+    This transformer performs quantile-based discretization on the input data.
+    The constructor function initializes the transformer with the number of quantiles (default is 10)
+    and optional labels for the bins.
+    """
+    def __init__(self, q = 10, labels = None):
+        """Init for the QCurTransformer class
+    
+        Args:
+            q (integer): number of bins; default is 10
+            labels (string list): list with the labels in the same lenght of q    
+        """
+        self.q = q  # Number of quantiles
+        self.labels = labels  # Labels for the bins
+        self.bins = None  # Will hold the bin edges after fit is called
+
+    def fit(self, X, y = None):
+        """Calculates the bin edges using pandas" qcut function
+        The `retbins` parameter is set to True so that qcut returns the bin edges in addition to the binned data.
+    
+        Args:
+            q (integer): number of bins
+            labels (string list): list with the labels in the same lenght of q    
+        """
+        _, self.bins = pd.qcut(X, q = self.q, retbins = True, duplicates="drop")
+        
+        return self
+
+    def transform(self, X, y = None):
+        """The transform function bins the input data using the bin edges calculated in fit.
+        If labels were provided, they are used to label the bins; otherwise, the bins are labeled with their numeric range.
+        We add -inf and inf to the bin edges to ensure that all data points are included in a bin.
+        If labels were provided, we add "below" and "above" labels for the -inf and inf bins.
+        
+        Args:
+            X (DataFrame column): values to be binned
+            
+        Returns:
+            the bins values of the input data
+        """
+        
+        extended_bins = np.hstack([[-np.inf], self.bins, [np.inf]])
+        
+        
+        if self.labels is not None:
+            extended_labels = np.hstack([["below"], self.labels, ["above"]])
+        else:
+            extended_labels = None
+
+        # We use pandas" cut function to bin the data using the extended bin edges and labels.
+        return pd.cut(X, bins = extended_bins, labels = extended_labels, include_lowest = True, duplicates = "drop")
